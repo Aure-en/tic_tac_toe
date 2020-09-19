16f54c3356319b132ba457d40715b1f571e42977
Aureen <yukiansff@gmail.com>
2020-09-19 15:44:34 +0200

    Fixed functions checking the state of the game

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const gameBoard = ((size) => {
 const player = (name, mark) => {
   let winCount = 0;
 
-  const play = (gameBoard, row, column) => {
+  const play = (row, column) => {
     if (gameBoard.isEmpty(row, column)) {
       gameBoard.board[row][column] = mark;
     }
@@ -90,7 +90,6 @@ const displayController = (() => {
 
   }
 
-
   const player = (player, number) => {
     document.querySelector(`.player${number} .player__name`).innerHTML = player.name;
     document.querySelector(`.player${number} .player__mark`).innerHTML = player.mark;
@@ -121,18 +120,98 @@ const gamePlay = (() => {
   const player1 = player("Player1", "x");
   const player2 = player("Player2", "o");
   let currentPlayer = player1;
-  let latestPlay;
+  let latestPlay = {};
 
   const _changePlayer = () => {
     currentPlayer = currentPlayer == player1 ? player2 : player1;
   }
 
-  const checkVictory = (board, player, cell) => {
+  const gameRound = (event) => {
+
+    latestPlay = { 
+      row : event.target.dataset.cell.split('-')[1],
+      column : event.target.dataset.cell.split('-')[2]
+    };
+
+    currentPlayer.play(latestPlay.row, latestPlay.column);
+    console.log(gameBoard.board);
+    document.querySelector(`[data-cell$="${latestPlay.row}-${latestPlay.column}"`).innerHTML = currentPlayer.mark;
+    console.log(currentPlayer);
+    console.log(gameChecks._checkRow(gameBoard.board, latestPlay.row, currentPlayer));
+    _changePlayer(player1, player2);
+  }
+
+  const gameInit = () => {
+    
+    //Render the empty board and players
+      displayController.board(gameBoard);
+      displayController.player(player1, 1);
+      displayController.player(player2, 2);
+
+    //Allows the players to click on the board to play the game.  
+    document.querySelector(".board").addEventListener("click", gameRound);
+
+  }
+    
+  return {
+    gameRound,
+    gameInit
+  };
+
+})();
+
+const gameChecks = (() => {
+
+  const _checkRow = (board, row, player) => {
+    for (let column = 0 ; column < Math.sqrt(board.length) ; column++) {
+      if (board[row][column] != player.mark) return false;
+    }
+    return true;
+  }
+
+  const _checkColumn = (board, column, player) => {
+    for (let row = 0 ; row < Math.sqrt(board.length) ; row++) {
+      if (board[row][column] != player.mark) return false;
+    }
+    return true;
+  }
+
+  const _checkDiagonal = (board, player) => {
+    for (let index = 0 ; index < Math.sqrt(board.length) ; index++) {
+      if (board[index][index] != player.mark) return false;
+    }
+    return true;
+  }
+
+  const _checkAntiDiagonal = (board, player) => {
+    for (let index = 0 ; index < Math.sqrt(board.length) ; index++) {
+        if (board[index][Math.sqrt(board.length) - 1 - index] != player.mark) return false;
+      }
+    return true;
   }
 
   const checkTie = (board) => {
     if (board.isFull()) return true;
     return false;
+  }
+
+  const checkVictory = (board, player, row, column) => {
+
+    if (row == column && row == Math.sqrt(board.length) - 1 - column) {
+      return _checkDiagonal(board, player) || _checkAntiDiagonal(board, player) || _checkRow(board, row, player) || _checkColumn(board, column, player);
+    }
+
+    else if (row == column) {
+      return _checkDiagonal(board, player) || _checkRow(board, row, player) || _checkColumn(board, column, player);
+    }
+
+    else if (row == Math.sqrt(board.length) - 1 - column) {
+      return _checkAntiDiagonal(board, player) || _checkRow(board, row, player) || _checkColumn(board, column, player);
+    }
+
+    else {
+      return _checkRow(board, row, player) || _checkColumn(board, column, player);
+    }
   }
 
   const _win = () => {
@@ -144,30 +223,14 @@ const gamePlay = (() => {
     displayController.tie();
   }
 
-  const gameRound = (event) => {
-    console.log(`${event.target.dataset.cell.split('-')[1]}-${event.target.dataset.cell.split('-')[2]}`);
-    currentPlayer.play(gameBoard, event.target.dataset.cell.split('-')[1], event.target.dataset.cell.split('-')[2]);
-    document.querySelector(`[data-cell$="${event.target.dataset.cell.split('-')[1]}-${event.target.dataset.cell.split('-')[2]}"`).innerHTML = currentPlayer.mark;
-    _changePlayer(player1, player2);
-  }
-
-  const gameInit = () => {
-    
-    //Render the empty board and players
-      displayController.board(gameBoard);
-      displayController.player(player1, 1);
-      displayController.player(player2, 2);
-
-    document.querySelector(".board").addEventListener("click", gameRound);
-
-  }
-    
   return {
-    checkVictory,
     checkTie,
-    gameRound,
-    gameInit
-  };
+    checkVictory,
+    _checkRow,
+    _checkColumn,
+    _checkDiagonal,
+    _checkAntiDiagonal
+  }
 
 })();
 
